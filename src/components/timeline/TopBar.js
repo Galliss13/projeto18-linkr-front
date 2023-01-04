48ace2fb1212485a57717fe14b4792c94291382b
carloshnp <carloshnpwork@gmail.com>
2023-01-03 22:45:06 -0300

    feat: topbar styles + logo

--- src/components/timeline/TopBar.js
+++ src/components/timeline/TopBar.js
@@ -1,44 +1,59 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export default function TopBar() {
-    return (
-        <Container>
-            <Logo>
-                <Linkr>Linkr</Linkr>
-            </Logo>
-            <UserContainer>
-                <DropdownList />
-                <UserImage />
-            </UserContainer>
-        </Container>
-    )
+  return (
+    <Container>
+      <Logo>
+        <Linkr>linkr</Linkr>
+      </Logo>
+      <UserContainer>
+        <DropdownList>
+            <h1>⌄</h1>
+        </DropdownList>
+        <UserImage />
+      </UserContainer>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    height: 72px;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-`
+  height: 72px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-color: #151515;
+`;
 
 const Logo = styled.div`
-    margin-left: 28px;
-    display: flex;
-    align-items: center;
-`
+  margin-left: 23px;
+  display: flex;
+  align-items: center;
+`;
 
 const Linkr = styled.h1`
-    font-family: 'Passion One', cursive;
-    font-size: 49px;
-`
+  font-family: "Passion One", cursive;
+  font-size: 49px;
+  color: #ffffff;
+`;
 
 const UserContainer = styled.div`
-    width: 90px;
-    margin-right: 28px;
-    display: flex;
-    align-items: center;
-`
+  width: 100px;
+  margin-right: 23px;
+  display: flex;
+  align-items: center;
+`;
 
-const DropdownList = styled.div``
+const DropdownList = styled.div`
+    margin-right: 10px;
+    h1 {
+        color: #ffffff;
+        font-size: 43px;
+    }
+`;
 
-const UserImage = styled.img``
+const UserImage = styled.img`
+    width: 53px;
+    height: 53px;
+    background-color: #000000;
+    border-radius: 30px;
+`;
