738d45dc9c6d164561d026fdc41fad44794249cd
Galliss13 <matheusgalindo1313@gmail.com>
2023-01-13 15:13:55 -0300

    fix: CommentsBox open optimized

--- src/components/timeline/CommentBox.js
+++ src/components/timeline/CommentBox.js
@@ -4,7 +4,7 @@ import UserImage from "../elements/UserImage";
 
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/Context";
-import { getComments, postComment } from "../../service/Service";
+import { getAllUserFollows, getComments, postComment } from "../../service/Service";
 import { IconContext } from "react-icons";
 import { FaRegPaperPlane } from "react-icons/fa";
 import { RotatingLines } from "react-loader-spinner";
@@ -15,6 +15,7 @@ export default function CommentBox(props) {
   const { id, postOwnerName } = props;
   const [comments, setComents] = useState("");
   const [text, setText] = useState("");
+  const [follows, setFollows] = useState("")
   const [commentsAreLoading, setCommentsAreLoading] = useState(false);
   const [IsNewUserCommentLoading, setIsNewUserCommentLoading] = useState(false);
   const [renderComments, setRenderComments] = useState(false);
@@ -31,6 +32,14 @@ export default function CommentBox(props) {
         setCommentsAreLoading(false);
         console.log(err.response.data);
       });
+
+    getAllUserFollows(`follows`, user.id)
+    .then((ans) => {
+      setFollows(ans.data)
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+    })
   }, [id, renderComments]);
 
   function handleSubmit(e) {
@@ -52,6 +61,17 @@ export default function CommentBox(props) {
 
   return (
     <Container>
+      {commentsAreLoading && (
+        <CommentsContainer>
+          <RotatingLines 
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="36"
+          visible={true}
+          />
+      </CommentsContainer>
+      )}
       {!commentsAreLoading && typeof comments !== "string" && (
         <CommentsContainer>
           {comments.map((c) => (
@@ -60,6 +80,7 @@ export default function CommentBox(props) {
               userName={name}
               commentText={c.text}
               postOwnerName={postOwnerName}
+              follows={follows}
             />
           ))}
         </CommentsContainer>
